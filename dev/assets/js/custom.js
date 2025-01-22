6d7867b207b591ff5c257be937142e5a24fb03c1
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2025-01-22 17:37:35 -0300

    added menu animation and more animation on page

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,16 +1,100 @@
-// $(".carousel").carousel({ ride: false });
+const menuNav = document.getElementsByClassName("navbar-nav");
+
+const menuOptions = [
+  {
+    classObserver: ".openMenu",
+    show: true,
+    intersectionRatio: 0.75,
+  },
+  {
+    classObserver: ".closeMenu",
+    show: false,
+    intersectionRatio: 0.75,
+  },
+];
+
+menuOptions.forEach((options) => {
+  const menuObserver = new IntersectionObserver(
+    (entries) => {
+      Array.from(entries).forEach((entry) => {
+        if (entry.intersectionRatio >= options.intersectionRatio) {
+          options.show
+            ? $(".navbar-nav").addClass("show")
+            : $(".navbar-nav").removeClass("show");
+        }
+      });
+    },
+    {
+      threshold: [0.25, 0.5, 0.75, 1],
+    }
+  );
+
+  Array.from(document.querySelectorAll(options.classObserver)).forEach(
+    (element) => {
+      menuObserver.observe(element);
+    }
+  );
+});
 
 const animations = [
   { class: "bounce", type: "bounce", delay: 2, speed: "slow", repeat: 1 },
-  { class: "fadeInDown", type: "fadeInDown", delay: 2, speed: "slow" },
-  // { type: "fadeInDown", delay: 2, speed: "slow", repeat: 1 },
+  { class: "fadeInDown", type: "fadeInDown", delay: 0, speed: "slow" },
+  {
+    class: "fadeInLeft",
+    type: "fadeInLeft",
+    delay: 0,
+    speed: "slow",
+    repeat: 1,
+  },
+  {
+    class: "fadeInRight",
+    type: "fadeInRight",
+    delay: 0,
+    speed: "slow",
+    repeat: 1,
+  },
+  {
+    class: "fadeInUp",
+    type: "fadeInUp",
+    delay: 0,
+    speed: "slow",
+    repeat: 1,
+  },
+  {
+    class: "appsAnimation-1",
+    type: "fadeInUp",
+    delay: 0,
+    speed: "slow",
+    repeat: 1,
+  },
+  {
+    class: "appsAnimation-2",
+    type: "fadeInUp",
+    delay: 0,
+    speed: "slower",
+    repeat: 1,
+  },
+  {
+    class: "appsAnimation-3",
+    type: "fadeInUp",
+    delay: 1,
+    speed: "slow",
+    repeat: 1,
+  },
+  {
+    class: "slowIn",
+    type: "slowIn",
+    delay: 1,
+    speed: "slow",
+    repeat: 1,
+  },
 ];
 
 animations.forEach((animation) => {
   const observer = new IntersectionObserver(
     (entries) => {
       Array.from(entries).forEach((entry) => {
-        if (entry.intersectionRatio >= 0.75) {
+        if (entry.intersectionRatio >= 0.5) {
           entry.target.classList.add(
             "animate__animated",
             `animate__${animation.type}`,
@@ -30,7 +114,6 @@ animations.forEach((animation) => {
   Array.from(document.querySelectorAll(`.${animation.class}`)).forEach(
     (element) => {
       observer.observe(element);
-      console.log("achou");
     }
   );
 });
